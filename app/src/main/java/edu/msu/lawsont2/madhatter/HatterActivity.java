e544f082581f5c51af0b45b8a85cf1a6277f65af
Tyler Lawson <lawsont2@cse.msu.edu>
2020-02-21 18:56:04 -0500

    Does image replace, but errors on devices

--- app/src/main/java/edu/msu/lawsont2/madhatter/HatterActivity.java
+++ app/src/main/java/edu/msu/lawsont2/madhatter/HatterActivity.java
@@ -1,9 +1,19 @@
 package edu.msu.lawsont2.madhatter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -37,6 +47,38 @@ public class HatterActivity extends AppCompatActivity {
         return (CheckBox)findViewById(R.id.checkFeather);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
+            // Response from the picture selection activity
+            Uri imageUri = data.getData();
+
+            // We have to query the database to determine the document ID for the image
+            Cursor cursor = getContentResolver().query(imageUri, null, null, null, null);
+            cursor.moveToFirst();
+            String document_id = cursor.getString(0);
+            document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+            cursor.close();
+
+            // Next, we query the content provider to find the path for this
+            // document id.
+            cursor = getContentResolver().query(
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+            cursor.moveToFirst();
+            String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            cursor.close();
+
+            if(path != null) {
+                Log.i("Path", path);
+                getHatterView().setImagePath(path);
+            }
+
+        }
+    }
+
     /**
      * The hat choice spinner
      */
@@ -48,6 +90,14 @@ public class HatterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hatter);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
     }
 
     /**
--- app/src/main/java/edu/msu/lawsont2/madhatter/HatterView.java
+++ app/src/main/java/edu/msu/lawsont2/madhatter/HatterView.java
@@ -1,5 +1,7 @@
 package edu.msu.lawsont2.madhatter;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
@@ -8,6 +10,23 @@ import android.view.View;
  * The view we will draw out hatter in
  */
 public class HatterView extends View {
+
+    private class Parameters {
+        /**
+         * Path to the image file if one exists
+         */
+        public String imagePath = null;
+    }
+
+    /**
+     * The current parameters
+     */
+    private Parameters params = new Parameters();
+
+    /**
+     * The image bitmap. None initially.
+     */
+    private Bitmap imageBitmap = null;
 
     public HatterView(Context context) {
         super(context);
@@ -32,7 +51,32 @@ public class HatterView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        // If there is no image to draw, we do nothing
+        if(imageBitmap == null) {
+            return;
+        }
 
+        canvas.drawBitmap(imageBitmap, 0, 0, null);
+    }
+
+
+    /**
+     * Get the installed image path
+     * @return path or null if none
+     */
+    public String getImagePath() {
+        return params.imagePath;
+    }
+
+    /**
+     * Set an image path
+     * @param imagePath path to image file
+     */
+    public void setImagePath(String imagePath) {
+        params.imagePath = imagePath;
+
+        imageBitmap = BitmapFactory.decodeFile(imagePath);
+        invalidate();
     }
 
 }
