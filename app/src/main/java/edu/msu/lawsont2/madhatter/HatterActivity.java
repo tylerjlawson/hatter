29d0cb832b9bdd4485414d0018834ea5be15d28f
Tyler Lawson <lawsont2@cse.msu.edu>
2020-02-20 23:23:59 -0500

    portrait layout done

--- app/src/main/java/edu/msu/lawsont2/madhatter/HatterActivity.java
+++ app/src/main/java/edu/msu/lawsont2/madhatter/HatterActivity.java
@@ -2,13 +2,64 @@ package edu.msu.lawsont2.madhatter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 
 public class HatterActivity extends AppCompatActivity {
+
+    /**
+     * Request code when selecting a picture
+     */
+    private static final int SELECT_PICTURE = 1;
+
+    /**
+     * The hatter view object
+     */
+    private HatterView getHatterView() {
+        return (HatterView) findViewById(R.id.hatterView);
+    }
+
+    /**
+     * The color select button
+     */
+    private Button getColorButton() {
+        return (Button)findViewById(R.id.buttonColor);
+    }
+
+    /**
+     * The feather checkbox
+     */
+    private CheckBox getFeatherCheck() {
+        return (CheckBox)findViewById(R.id.checkFeather);
+    }
+
+    /**
+     * The hat choice spinner
+     */
+    private Spinner getSpinner() {
+        return (Spinner) findViewById(R.id.spinnerHat);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hatter);
+    }
+
+    /**
+     * Handle a Picture button press
+     * @param view
+     */
+    public void onPicture(View view) {
+        // Get a picture from the gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
     }
 }
